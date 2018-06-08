6534a9df2fe5c99df6a4220252e2ae789cdd9582
justindevpro <justin@itpro.tv>
2018-06-08 13:09:08 -0400

    fixed formatting

--- raindrops/raindrops.go
+++ raindrops/raindrops.go
@@ -6,25 +6,23 @@ type Factor struct {
 	label string
 	value int
 }
-var factors = []Factor{
-	Factor{ label: "Pling", value: 3}, 
-	Factor{ label: "Plang", value: 5}, 
-	Factor{ label: "Plong", value: 7},
-}
 
+var factors = []Factor{
+	Factor{label: "Pling", value: 3},
+	Factor{label: "Plang", value: 5},
+	Factor{label: "Plong", value: 7},
+}
 
 func Convert(num int) string {
 	result := ""
 	for _, factor := range factors {
-		if num % factor.value == 0 {
+		if num%factor.value == 0 {
 			result += factor.label
-		}	
+		}
 	}
-	if result == ""{
+	if result == "" {
 		return fmt.Sprintf("%d", num)
 	}
 	return result
-	
+
 }
-
-
