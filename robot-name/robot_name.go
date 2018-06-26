85d01a2e0ef9538569a5ffdf0502f055a0b757b1
justindevpro <justin@itpro.tv>
2018-06-26 16:08:21 -0400

    finished robot name

---
+++ robot-name/robot_name.go
@@ -0,0 +1,46 @@
+package robotname
+
+import (
+	"math/rand"
+	"strings"
+)
+
+const nameLength int = 5
+const letterLength int = 2
+const digitLength int = nameLength - letterLength
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const digits = "0123456789"
+
+type Robot struct {
+	name string
+}
+
+func (r *Robot) Name() string {
+	if r.name != "" {
+		return r.name
+	}
+	result := make([]string, nameLength)
+
+	for i := 0; i < letterLength; i++ {
+		r := getRandomChar(letters)
+		result = append(result, r)
+	}
+
+	for i := 0; i < digitLength; i++ {
+		d := getRandomChar(digits)
+		result = append(result, d)
+	}
+	name := strings.Join(result, "")
+	r.name = name
+	return name
+}
+
+func (r *Robot) Reset() {
+	r.name = ""
+}
+
+func getRandomChar(source string) string {
+	rs := strings.Split(source, "")
+	i := rand.Intn(len(rs))
+	return rs[i]
+}
